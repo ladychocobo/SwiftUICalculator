24853b2f014257a3dd6f65a53c53565a2ac58a22
Jamie <jamie.yjchen@gmail.com>
2020-06-25 14:02:15 +0800

    init Project

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by Jamie Chen on 2020/6/25.
+//  Copyright © 2020 Jamie Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
