94a9ae0a12d5683662d10d38273d42caa1465dc7
Jamie <jamie.yjchen@gmail.com>
2020-06-25 15:58:36 +0800

    add calculator ui

--- SwiftUICalculator/ContentView.swift
+++ SwiftUICalculator/ContentView.swift
@@ -9,8 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let buttonText = [
+        ["AC", "±", "%", "÷"],
+        ["7", "8", "9", "⨉"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+
+    var buttonSize: CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+
     var body: some View {
-        Text("Hello, World!")
+
+        ZStack (alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+
+            VStack (spacing: 10) {
+                HStack {
+                    Spacer()
+                    Text("43").foregroundColor(.white).font(.system(size: 64)).padding()
+                }
+
+                ForEach(buttonText, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { element in
+                            Text(element)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonSize, height: self.buttonSize, alignment: .center)
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonSize/2)
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
     }
 }
 
